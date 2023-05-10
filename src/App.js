baccee2fbac39146db65c25bbe0c123f8eb0edbc
Joanna Booth <118810624+stillawake17@users.noreply.github.com>
2023-05-10 20:02:03 +0100

    react server
    react server

---
+++ src/App.js
@@ -0,0 +1,36 @@
+
+import * as React from 'react';
+
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home'; 
+import Contact from './pages/Contact'; 
+import Header from './components/Header';
+import NotFound from './pages/NotFound';
+import ProjectGallery from './components/ProjectGallery';
+import Project from './components/Project';
+import Book from './components/Book';
+
+
+
+
+function App() {
+  const title = 'Portfolio';
+  return (
+    <>
+    <Header title={title}/>
+    <Routes>
+        <Route path="/" element={<Home title={title} />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/projectGallery" element={<ProjectGallery />} />
+        {/* <Route path="/project" element={<Project />} /> */}
+        <Route path="/book" element={<Book />} />
+    </Routes>
+   
+    
+      
+    </>
+    )
+  }
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function Header(props) {
+    return (                
+     <header>
+      <h3>{props.title}</h3>
+        <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+        <NavLink to="/projectGallery">Projects</NavLink>
+        {/* <NavLink to="/project">Project</NavLink> */}
+        <NavLink to="/book">Books</NavLink>
+        </nav>
+        </header>
+         )};
+         
+    export default Header;
